9754cf61f9962b91da5d730f67f979ac5a23eca8
littleleek <little.leekchan@gmail.com>
2019-05-05 22:14:11 +0100

    Chapter 23: Using Reactive Extensions.

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Product } from '../model/product.model';
 import { Model } from '../model/repository.model';
-import { MODES, SharedState } from './sharedState.model';
+import { MODES, SharedState, SHARED_STATE } from './sharedState.model';
+import { Observable } from 'rxjs';
+import { filter, map, distinctUntilChanged, skipWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'paForm',
@@ -11,12 +13,26 @@ import { MODES, SharedState } from './sharedState.model';
 })
 export class FormComponent {
   product: Product = new Product();
+  lastId: number;
 
-  constructor(private model: Model, private state: SharedState) { }
+  constructor(
+    private model: Model,
+    @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
 
-  get editing(): boolean {
-    return this.state.mode == MODES.EDIT;
+    stateEvents
+      .pipe(skipWhile(state => state.mode == MODES.EDIT))
+      .pipe(distinctUntilChanged((firstState, secondState) =>
+        firstState.mode == secondState.mode && firstState.id == secondState.id))
+      .subscribe(update => {
+        this.product == new Product();
+        if (update.id != undefined) {
+          Object.assign(this.product, this.model.getProduct(update.id));
+        }
+        this.editing = update.mode == MODES.EDIT;
+      });
   }
+
+  editing: boolean = false;
 
   submitForm(form: NgForm) {
     if (form.valid) {
